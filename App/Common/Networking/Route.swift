e60760c69777cf84b13a739f2a3afa03957a078a
Luiz Hartmann <luiz-hartmann@outlook.com>
2022-11-18 15:01:50 -0400

    created route

---
+++ App/Common/Networking/Route.swift
@@ -0,0 +1,35 @@
+//
+//  Route.swift
+//  App
+//
+//  Created by Luiz Hartmann on 18/11/22.
+//
+
+import Foundation
+
+protocol Route {
+    var baseURL: String { get }
+    var path: String { get }
+    var parameters: Any? { get }
+    var method: HTTPMethod { get }
+}
+
+extension Route {
+    
+    var url: URL {
+        return URL(string: baseURL + path)!
+    }
+    
+    var dataParameters: Data? {
+        guard let parameters = parameters else { return nil }
+        let data = try? JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+        return data
+    }
+    
+    var request: URLRequest {
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.httpBody = dataParameters
+        return request
+    }
+}
