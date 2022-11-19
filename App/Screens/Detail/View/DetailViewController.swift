f70b060c09bd8b7c02407dce64f9fd00500d5b3b
Luiz Hartmann <luiz-hartmann@outlook.com>
2022-11-19 10:57:42 -0400

    created title detail label

--- App/Screens/Detail/View/DetailViewController.swift
+++ App/Screens/Detail/View/DetailViewController.swift
@@ -19,6 +19,15 @@ class DetailViewController: UIViewController {
         configure()
     }
     
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Especificações:"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 28, weight: .semibold)
+        return label
+    }()
+    
     private lazy var brandNameLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -54,7 +63,7 @@ class DetailViewController: UIViewController {
         return label
     }()
     
-    lazy var saveButton: UIButton = {
+    private lazy var saveButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Eu quero!", for: .normal)
@@ -67,9 +76,8 @@ class DetailViewController: UIViewController {
         return button
     }()
     
-     @objc private func saveButtonTap() {
+    @objc private func saveButtonTap() {
         self.coordinator?.onSave()
-        
     }
     
     private lazy var stackView: UIStackView = {
@@ -98,21 +106,27 @@ class DetailViewController: UIViewController {
 extension DetailViewController: Viewcode {
     
     func buildViewHierarchy() {
+        
         view.addSubview(stackView)
         view.addSubview(saveButton)
+        view.addSubview(titleLabel)
     }
     
     func addConstraints() {
         NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            
             stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             
-            saveButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 18),
+            saveButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 50),
             saveButton.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 70),
             saveButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: -70),
-            saveButton.heightAnchor.constraint(equalToConstant: 40)
+            saveButton.heightAnchor.constraint(equalToConstant: 45)
         ])
     }
     
